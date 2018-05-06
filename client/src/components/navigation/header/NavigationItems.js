0b50628f442b1fb514388d0e667865d4563a90a6
zirui-wang <zirui.zirui.wang.wang@gmail.com>
2018-05-06 00:21:12 -0700

    responsive

--- client/src/components/navigation/header/NavigationItems.js
+++ client/src/components/navigation/header/NavigationItems.js
@@ -1,54 +1,67 @@
 import React from 'react';
-
-import Button from '../../UI/Button';
 import { Link } from 'react-router-dom';
 
-const navigationItems = props => (
-  <div>
-    <Button
-      myProps={{
-        to: '/',
-        color: 'inherit',
-        size: 'medium',
-        component: Link
-      }}
-    >
-      Burger Builder
-    </Button>
-    {props.isAuthenticated ? (
+import { withStyles } from 'material-ui/styles';
+
+import Button from '../../UI/Button';
+
+const styles = {
+  deskTopOnly: {
+    '@media (max-width: 499px)': {
+      display: 'none'
+    }
+  }
+};
+
+const navigationItems = props => {
+  const {classes} = props;
+  return (
+    <div className={classes.deskTopOnly}>
       <Button
         myProps={{
-          to: '/orders',
+          to: '/',
           color: 'inherit',
           size: 'medium',
           component: Link
         }}
       >
-        Orders
+        Burger Builder
       </Button>
-    ) : null}
-    {!props.isAuthenticated ? (
-      <Button
-        myProps={{
-          href: '/auth/google',
-          color: 'inherit',
-          size: 'medium',
-        }}
-      >
-        Login with Google
-      </Button>
-    ) : (
-      <Button
-        myProps={{
-          href: 'api/logout',
-          color: 'inherit',
-          size: 'medium',
-        }}
-      >
-        Logout
-      </Button>
-    )}
-  </div>
-);
+      {props.isAuthenticated ? (
+        <Button
+          myProps={{
+            to: '/orders',
+            color: 'inherit',
+            size: 'medium',
+            component: Link
+          }}
+        >
+          Orders
+        </Button>
+      ) : null}
+      {!props.isAuthenticated ? (
+        <Button
+          myProps={{
+            href: '/auth/google',
+            color: 'inherit',
+            size: 'medium'
+          }}
+        >
+          Login with Google
+        </Button>
+      ) : (
+        <Button
+          myProps={{
+            href: 'api/logout',
+            color: 'inherit',
+            size: 'medium'
+          }}
+        >
+          Logout
+        </Button>
+      )}
+    </div>
+  )
+}
 
-export default navigationItems;
+export default withStyles(styles)(navigationItems);
